b93e9b0d7aece1de5ef8aae5383bcad8250fac7c
donnie4w <donnie4w@gmail.com>
2024-09-04 23:54:51 +0800

    Update hstest1.go

--- dao/hstest1.go
+++ dao/hstest1.go
@@ -5,128 +5,82 @@
 //
 // github.com/donnie4w/gdao
 //
-// datetime :2024-08-03 12:22:44
+// datetime :2024-08-16 13:52:41
 // gdao version 1.1.0
-// dbtype:mysql ,database:hstest ,tablename:hstest1
+// dbtype:sqlite ,database:hstest.db ,tablename:hstest1
 
 package dao
 
 import (
 	"fmt"
-	"github.com/donnie4w/gdao/base"
 	"github.com/donnie4w/gdao"
+	"github.com/donnie4w/gdao/base"
 	
 )
-type hstest1_Id[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *int64
-}
-
-func (t *hstest1_Id[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *hstest1_Id[T]) Value() any {
-	return t.fieldValue
-}
-
-type hstest1_Rowname[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue *string
-}
-
-func (t *hstest1_Rowname[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *hstest1_Rowname[T]) Value() any {
-	return t.fieldValue
-}
-
-type hstest1_Value[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue []byte
-}
-
-func (t *hstest1_Value[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *hstest1_Value[T]) Value() any {
-	return t.fieldValue
-}
-
-type hstest1_Goto[T any] struct {
-	base.Field[T]
-	fieldName  string
-	fieldValue []byte
-}
-
-func (t *hstest1_Goto[T]) Name() string {
-	return t.fieldName
-}
-
-func (t *hstest1_Goto[T]) Value() any {
-	return t.fieldValue
-}
 
 type Hstest1 struct {
 	gdao.Table[Hstest1]
 
-	Id		*hstest1_Id[Hstest1]
-	Rowname		*hstest1_Rowname[Hstest1]
-	Value		*hstest1_Value[Hstest1]
-	Goto		*hstest1_Goto[Hstest1]
+	ID      *base.Field[Hstest1]
+	ROWNAME      *base.Field[Hstest1]
+	VALUE      *base.Field[Hstest1]
+	GOTO      *base.Field[Hstest1]
+	_ID      *int64
+	_ROWNAME      *string
+	_VALUE      []byte
+	_GOTO      []byte
 }
 
+var _Hstest1_ID = &base.Field[Hstest1]{"id"}
+var _Hstest1_ROWNAME = &base.Field[Hstest1]{"rowname"}
+var _Hstest1_VALUE = &base.Field[Hstest1]{"value"}
+var _Hstest1_GOTO = &base.Field[Hstest1]{"goto"}
+
 func (u *Hstest1) GetId() (_r int64){
-	if u.Id.fieldValue != nil {
-		_r = *u.Id.fieldValue
+	if u._ID != nil {
+		_r = *u._ID
 	}
 	return
 }
 
 func (u *Hstest1) SetId(arg int64) *Hstest1{
-	u.Put0(u.Id.fieldName, arg)
-	u.Id.fieldValue = &arg
+	u.Put0(u.ID.FieldName, arg)
+	u._ID = &arg
 	return u
 }
 
 func (u *Hstest1) GetRowname() (_r string){
-	if u.Rowname.fieldValue != nil {
-		_r = *u.Rowname.fieldValue
+	if u._ROWNAME != nil {
+		_r = *u._ROWNAME
 	}
 	return
 }
 
 func (u *Hstest1) SetRowname(arg string) *Hstest1{
-	u.Put0(u.Rowname.fieldName, arg)
-	u.Rowname.fieldValue = &arg
+	u.Put0(u.ROWNAME.FieldName, arg)
+	u._ROWNAME = &arg
 	return u
 }
 
 func (u *Hstest1) GetValue() (_r []byte){
-	_r = u.Value.fieldValue
+	_r = u._VALUE
 	return
 }
 
 func (u *Hstest1) SetValue(arg []byte) *Hstest1{
-	u.Put0(u.Value.fieldName, arg)
-	u.Value.fieldValue = arg
+	u.Put0(u.VALUE.FieldName, arg)
+	u._VALUE = arg
 	return u
 }
 
 func (u *Hstest1) GetGoto() (_r []byte){
-	_r = u.Goto.fieldValue
+	_r = u._GOTO
 	return
 }
 
 func (u *Hstest1) SetGoto(arg []byte) *Hstest1{
-	u.Put0(u.Goto.fieldName, arg)
-	u.Goto.fieldValue = arg
+	u.Put0(u.GOTO.FieldName, arg)
+	u._GOTO = arg
 	return u
 }
 
@@ -145,8 +99,7 @@ func (u *Hstest1) Scan(fieldname string, value any) {
 }
 
 func (t *Hstest1) ToGdao() {
-	_t := NewHstest1()
-	*t = *_t
+	t.init("hstest1")
 }
 
 func (t *Hstest1) Copy(h *Hstest1) *Hstest1{
@@ -161,26 +114,21 @@ func (t *Hstest1) String() string {
 	return fmt.Sprint("Id:",t.GetId(), ",","Rowname:",t.GetRowname(), ",","Value:",t.GetValue(), ",","Goto:",t.GetGoto())
 }
 
+func (t *Hstest1)init(tablename string) {
+	t.ID = _Hstest1_ID
+	t.ROWNAME = _Hstest1_ROWNAME
+	t.VALUE = _Hstest1_VALUE
+	t.GOTO = _Hstest1_GOTO
+	t.Init(tablename, []base.Column[Hstest1]{t.ID,t.ROWNAME,t.VALUE,t.GOTO})
+}
+
 func NewHstest1(tablename ...string) (_r *Hstest1) {
-
-	id := &hstest1_Id[Hstest1]{fieldName: "id"}
-	id.Field.FieldName = "id"
-
-	rowname := &hstest1_Rowname[Hstest1]{fieldName: "rowname"}
-	rowname.Field.FieldName = "rowname"
-
-	value := &hstest1_Value[Hstest1]{fieldName: "value"}
-	value.Field.FieldName = "value"
-
-	goto_ := &hstest1_Goto[Hstest1]{fieldName: "goto"}
-	goto_.Field.FieldName = "goto"
-
-	_r = &Hstest1{Id:id,Rowname:rowname,Value:value,Goto:goto_}
+	_r = &Hstest1{}
 	s := "hstest1"
 	if len(tablename) > 0 && tablename[0] != "" {
 		s = tablename[0]
 	}
-	_r.Init(s, []base.Column[Hstest1]{id,rowname,value,goto_})
+	_r.init(s)
 	return
 }
 
